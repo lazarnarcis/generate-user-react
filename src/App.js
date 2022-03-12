f31f3c385c9ac5f367e5b2201a99e1e3e02e49b0
Narcis Lazar <lnarcis310@gmail.com>
2022-03-12 22:02:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const api = "https://randomuser.me/api/";
+    const [stire, setStire] = useState([]);
+    const [map, setMap] = useState();
+
+    useEffect(() => {
+        axios(api)
+            .then(res => {
+                console.log(res.data.results[0])
+                setStire(res.data.results)
+            });
+    }, []);
+
+    useEffect(() => {
+        setMap(stire.map((stire1, key) => {
+            return <div key={key} className={ stire1.gender }>
+                <img src={stire1.picture.large} />
+                <h1>{ `${stire1.name.first} ${stire1.name.last}` }</h1>
+                <p>{ stire1.email }</p>
+            </div>
+        }));
+    }, [stire]);
+
+    return (
+        <>
+            { map }
+        </>
+    )
 }
 
 export default App;
